9b1cf2fc4dea3a0cb03306a613d52c6becd5734b
guess_ana <32547479+Gesana@users.noreply.github.com>
2021-05-16 22:30:20 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Navigation from "./components/navigation";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
